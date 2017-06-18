9ec9ce8303cf7fb185c03fd5f2ff16470984e8a2
Ubuntu <patryk.kudas13@gmail.com>
2017-06-18 19:04:43 +0000

    Init

---
+++ src/java/pl/edu/agh/tkk17/sample/NodeSub.java
@@ -0,0 +1,15 @@
+package pl.edu.agh.tkk17.sample;
+
+public class NodeSub extends Node
+{
+    public NodeSub(Node left, Node right)
+    {
+        super(left, right);
+    }
+
+    @Override
+    public void accept(NodeVisitor visitor)
+    {
+        visitor.visit(this);
+    }
+}
